57e181144018457cf7c3df04388df01a28eaf1d2
AS SH <sushmaawaghmare@gmail.com>
2018-08-12 17:01:54 +0530

    additional files

--- ecom/hello.py
+++ ecom/hello.py
@@ -3,4 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is {}</p>'.format(user_agent)
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
+
