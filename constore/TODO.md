c3b78a7c143a80376b7fc64e9750aae624a16512
AS SH <sushmaawaghmare@gmail.com>
2018-10-22 08:51:05 +0530

    trying async with flask

---
+++ constore/TODO.md
@@ -0,0 +1,1 @@
+1. fix template and see flask debugger
---
+++ constore/manage.py
@@ -0,0 +1,13 @@
+from flask_script import Manager
+from app import app
+
+manager = Manager(app)
+
+@manager.shell
+def make_shell_context():
+    """  Creates python REPL 
+    """
+    return dict(app=app)
+
+if __name__ == "__main__":
+    manager.run()
